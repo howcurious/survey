06674a52f880d0b27188be13c78980a18d81e5fd
howcurious <xd_007585@sina.com>
2022-05-19 11:29:54 +0800

    modify /NCoV/findDprtStat, /NCoV/findGrpStat ~

--- src/main/java/cn/nbbandxdd/survey/ncov/repository/NCoVRepository.java
+++ src/main/java/cn/nbbandxdd/survey/ncov/repository/NCoVRepository.java
@@ -13,24 +13,24 @@ public interface NCoVRepository extends ReactiveCrudRepository<NCoVEntity, Strin
     @Query("""
         ( \
             SELECT '天津研发部' AS DPRT_NAM, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS ON_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS OFF_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS OFF_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS OFF_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS ON_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS OFF_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS OFF_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS OFF_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V \
             FROM NCoV WHERE DPRT_NAM IN ('天研开发一部', '天研开发二部', '天研开发三部', '天研测试部', '天研综合管理部') \
         ) \
             UNION ALL \
@@ -41,24 +41,24 @@ public interface NCoVRepository extends ReactiveCrudRepository<NCoVEntity, Strin
             FROM (SELECT DPRT_NAM, MIN(SEQ_NO) AS SEQ_NO FROM GRP_INFO WHERE DPRT_NAM IN ('天研开发一部', '天研开发二部', '天研开发三部', '天研测试部', '天研综合管理部') GROUP BY DPRT_NAM ORDER BY SEQ_NO) T1 \
             LEFT JOIN (\
                 SELECT DPRT_NAM, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS ON_CNT_I, \
-                    IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS OFF_CNT_I, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_I, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_I, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_I, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_I, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_CNT_E, \
-                    IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS OFF_CNT_E, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_E, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_E, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_E, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_E, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_CNT_V, \
-                    IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS OFF_CNT_V, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_V, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_V, \
-                    IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS ON_CNT_I, \
+                    IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS OFF_CNT_I, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_I, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_I, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_I, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_I, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_CNT_E, \
+                    IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS OFF_CNT_E, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_E, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_E, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_E, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_E, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_CNT_V, \
+                    IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS OFF_CNT_V, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_V, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_V, \
+                    IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V \
                 FROM NCoV WHERE DPRT_NAM IS NOT NULL GROUP BY DPRT_NAM \
             ) T2 ON T1.DPRT_NAM = T2.DPRT_NAM \
             ORDER BY T1.SEQ_NO \
@@ -74,24 +74,24 @@ public interface NCoVRepository extends ReactiveCrudRepository<NCoVEntity, Strin
         FROM (SELECT DPRT_NAM, GRP_NAM, SEQ_NO FROM GRP_INFO WHERE DPRT_NAM = :dprtNam ORDER BY SEQ_NO) T1 \
         LEFT JOIN ( \
             SELECT DPRT_NAM, GRP_NAM, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS ON_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS OFF_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND HAM_CD LIKE 'TY%' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS OFF_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS OFF_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND HAM_CD NOT LIKE 'TY%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS ON_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS OFF_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS OFF_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 NOT LIKE '到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS OFF_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V \
             FROM NCoV WHERE DPRT_NAM = :dprtNam AND GRP_NAM IS NOT NULL GROUP BY GRP_NAM \
         ) T2 ON T1.DPRT_NAM = T2.DPRT_NAM AND T1.GRP_NAM = T2.GRP_NAM \
         ORDER BY T1.SEQ_NO \
@@ -101,6 +101,6 @@ public interface NCoVRepository extends ReactiveCrudRepository<NCoVEntity, Strin
     @Query("SELECT DPRT_NAM, GRP_NAM, USR_NAM, HAM_CD, Q01 FROM NCoV WHERE DPRT_NAM = :dprtNam AND GRP_NAM = :grpNam ORDER BY HAM_CD")
     Flux<NCoVDetailEntity> findDetail(String dprtNam, String grpNam);
 
-    @Query("SELECT COUNT(1) FROM NCoV WHERE OPEN_ID = :openId AND HAM_CD LIKE 'TY%'")
+    @Query("SELECT COUNT(1) FROM NCoV WHERE OPEN_ID = :openId AND FLAG = 'I'")
     Mono<Integer> findCntByOpenIdAndHamCd(String openId);
 }
