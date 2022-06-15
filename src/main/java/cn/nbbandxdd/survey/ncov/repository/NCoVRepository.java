3935874bc708931c1e84b8eba945ceb7c362f3b2
howcurious <xd_007585@sina.com>
2022-06-15 09:05:46 +0800

    modify sth hard coding...

--- src/main/java/cn/nbbandxdd/survey/ncov/repository/NCoVRepository.java
+++ src/main/java/cn/nbbandxdd/survey/ncov/repository/NCoVRepository.java
@@ -31,14 +31,14 @@ public interface NCoVRepository extends ReactiveCrudRepository<NCoVEntity, Strin
                 IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '因居住地临时要求%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS TEMP_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V \
-            FROM NCoV WHERE DPRT_NAM IN ('天研开发一部', '天研开发二部', '天研开发三部', '天研测试部', '天研综合管理部') \
+            FROM NCoV WHERE DPRT_NAM IN ('天研开发一部', '天研开发二部', '天研应用平台研发部', '天研测试部', '天研综合管理部') \
         ) \
             UNION ALL \
         ( \
             SELECT T1.DPRT_NAM, T2.ON_CNT_I, T2.OFF_CNT_I, T2.LEAVE_CNT_I, T2.AWAY_CNT_I, T2.TEMP_CNT_I, T2.GRAY_CNT_I, \
                 T2.ON_CNT_E, T2.OFF_CNT_E, T2.LEAVE_CNT_E, T2.AWAY_CNT_E, T2.TEMP_CNT_E, T2.GRAY_CNT_E, \
                 T2.ON_CNT_V, T2.OFF_CNT_V, T2.LEAVE_CNT_V, T2.AWAY_CNT_V, T2.TEMP_CNT_V, T2.GRAY_CNT_V \
-            FROM (SELECT DPRT_NAM, MIN(SEQ_NO) AS SEQ_NO FROM GRP_INFO WHERE DPRT_NAM IN ('天研开发一部', '天研开发二部', '天研开发三部', '天研测试部', '天研综合管理部') GROUP BY DPRT_NAM ORDER BY SEQ_NO) T1 \
+            FROM (SELECT DPRT_NAM, MIN(SEQ_NO) AS SEQ_NO FROM GRP_INFO WHERE DPRT_NAM IN ('天研开发一部', '天研开发二部', '天研应用平台研发部', '天研测试部', '天研综合管理部') GROUP BY DPRT_NAM ORDER BY SEQ_NO) T1 \
             LEFT JOIN (\
                 SELECT DPRT_NAM, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '到岗%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS ON_CNT_I, \
