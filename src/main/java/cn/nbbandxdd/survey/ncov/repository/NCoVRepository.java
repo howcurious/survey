9a059f601f4df08adb1fd8c4327777b41f3e6ac1
howcurious <xd_007585@sina.com>
2022-12-23 11:34:10 +0800

    bug fix... /NCoV/findDprtStat /NCoV/findGrpStat ~

--- src/main/java/cn/nbbandxdd/survey/ncov/repository/NCoVRepository.java
+++ src/main/java/cn/nbbandxdd/survey/ncov/repository/NCoVRepository.java
@@ -19,24 +19,24 @@ public interface NCoVRepository extends ReactiveCrudRepository<NCoVEntity, Strin
                 IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_I, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_I, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q12 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS APOS_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q11 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q18 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS APOS_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q17 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_I, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '阳康到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_POS_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 NOT LIKE '%到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS OFF_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q12 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS APOS_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q11 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q18 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS APOS_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q17 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '阳康到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_POS_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 NOT LIKE '%到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS OFF_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q12 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS APOS_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q11 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_V \
+                IFNULL(SUM(CASE WHEN Q18 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS APOS_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q17 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_V \
             FROM NCoV WHERE DPRT_NAM IN ('天研开发一部', '天研开发二部', '天研应用平台研发部', '天研测试部', '天研综合管理部') \
         ) \
             UNION ALL \
@@ -54,24 +54,24 @@ public interface NCoVRepository extends ReactiveCrudRepository<NCoVEntity, Strin
                     IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_I, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_I, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_I, \
-                    IFNULL(SUM(CASE WHEN Q12 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS APOS_CNT_I, \
-                    IFNULL(SUM(CASE WHEN Q11 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_I, \
+                    IFNULL(SUM(CASE WHEN Q18 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS APOS_CNT_I, \
+                    IFNULL(SUM(CASE WHEN Q17 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_I, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '%到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_CNT_E, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '阳康到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_POS_CNT_E, \
                     IFNULL(SUM(CASE WHEN Q01 NOT LIKE '%到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS OFF_CNT_E, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_E, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_E, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_E, \
-                    IFNULL(SUM(CASE WHEN Q12 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS APOS_CNT_E, \
-                    IFNULL(SUM(CASE WHEN Q11 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_E, \
+                    IFNULL(SUM(CASE WHEN Q18 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS APOS_CNT_E, \
+                    IFNULL(SUM(CASE WHEN Q17 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_E, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '%到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_CNT_V, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '阳康到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_POS_CNT_V, \
                     IFNULL(SUM(CASE WHEN Q01 NOT LIKE '%到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS OFF_CNT_V, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_V, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
                     IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V, \
-                    IFNULL(SUM(CASE WHEN Q12 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS APOS_CNT_V, \
-                    IFNULL(SUM(CASE WHEN Q11 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_V \
+                    IFNULL(SUM(CASE WHEN Q18 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS APOS_CNT_V, \
+                    IFNULL(SUM(CASE WHEN Q17 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_V \
                 FROM NCoV WHERE DPRT_NAM IS NOT NULL GROUP BY DPRT_NAM \
             ) T2 ON T1.DPRT_NAM = T2.DPRT_NAM \
             ORDER BY T1.SEQ_NO \
@@ -93,31 +93,31 @@ public interface NCoVRepository extends ReactiveCrudRepository<NCoVEntity, Strin
                 IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_I, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_I, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q12 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS APOS_CNT_I, \
-                IFNULL(SUM(CASE WHEN Q11 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q18 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS APOS_CNT_I, \
+                IFNULL(SUM(CASE WHEN Q17 = '阳性' AND FLAG = 'I' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_I, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '阳康到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS ON_POS_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 NOT LIKE '%到岗%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS OFF_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q12 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS APOS_CNT_E, \
-                IFNULL(SUM(CASE WHEN Q11 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q18 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS APOS_CNT_E, \
+                IFNULL(SUM(CASE WHEN Q17 = '阳性' AND FLAG = 'E' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_E, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '阳康到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS ON_POS_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 NOT LIKE '%到岗%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS OFF_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '请假%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS LEAVE_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '离开驻地%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS AWAY_CNT_V, \
                 IFNULL(SUM(CASE WHEN Q01 LIKE '%（涉及灰名单）%' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS GRAY_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q12 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS APOS_CNT_V, \
-                IFNULL(SUM(CASE WHEN Q11 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_V \
+                IFNULL(SUM(CASE WHEN Q18 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS APOS_CNT_V, \
+                IFNULL(SUM(CASE WHEN Q17 = '阳性' AND FLAG = 'V' THEN 1 ELSE 0 END), 0) AS NPOS_CNT_V \
             FROM NCoV WHERE DPRT_NAM = :dprtNam AND GRP_NAM IS NOT NULL GROUP BY GRP_NAM \
         ) T2 ON T1.DPRT_NAM = T2.DPRT_NAM AND T1.GRP_NAM = T2.GRP_NAM \
         ORDER BY T1.SEQ_NO \
         """)
     Flux<NCoVStatEntity> findGrpStat(String dprtNam);
 
-    @Query("SELECT DPRT_NAM, GRP_NAM, USR_NAM, HAM_CD, (CASE WHEN Q01 LIKE '涉疫%' THEN CONCAT('涉疫（抗原：', IFNULL(Q12, '暂无'), '，核酸：', IFNULL(Q11, '暂无'), '）') ELSE Q01 END) AS Q01 FROM NCoV WHERE DPRT_NAM = :dprtNam AND GRP_NAM = :grpNam ORDER BY HAM_CD")
+    @Query("SELECT DPRT_NAM, GRP_NAM, USR_NAM, HAM_CD, (CASE WHEN Q01 LIKE '涉疫%' THEN CONCAT('涉疫（抗原：', IFNULL(Q18, '暂无'), '，核酸：', IFNULL(Q17, '暂无'), '）') ELSE Q01 END) AS Q01 FROM NCoV WHERE DPRT_NAM = :dprtNam AND GRP_NAM = :grpNam ORDER BY HAM_CD")
     Flux<NCoVDetailEntity> findDetail(String dprtNam, String grpNam);
 
     @Query("SELECT COUNT(1) FROM NCoV WHERE OPEN_ID = :openId AND FLAG = 'I'")
